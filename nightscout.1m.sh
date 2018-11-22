8224dc9d0ec855ffd676624319d2d7fa631b5fa0
JHD <jhaydraude@salesforce.com>
2018-11-22 13:47:56 -0500

    Fixes required by upstream LINT

--- nightscout.1m.sh
+++ nightscout.1m.sh
@@ -1,60 +1,58 @@
 #!/bin/bash
 # <bitbar.title>Nightscout Reader</bitbar.title>
-# <bitbar.version>0.0.1</bitbar.version>
-# <bitbar.author>Jeremy Hay Draude</bitbar.author>
-# <bitbar.desc>Display current Nightscout Blood Sugar and Trend</bitbar.desc>
+# <bitbar.version>0.1.0</bitbar.version>
+# <bitbar.desc>For Diabetics using Nightscout to track CGM data: Display current Blood Sugar data and trend from Nightscout</bitbar.desc>
 # <bitbar.dependencies>bash, curl, bc</bitbar.dependencies>
 # <bitbar.author.github>badgerpapa</bitbar.author.github>
 # <bitbar.image>https://raw.githubusercontent.com/badgerpapa/NightscoutBitBar/master/Preview.png</bitbar.image>
 # <bitbar.abouturl>https://github.com/badgerpapa/NightscoutBitBar/blob/master/README.md</bitbar.abouturl>
 
-NSURL=http://YOURNIGHTSCOUTURL.herokuapp.com
-USEMMOL=true
+NSURL=http://YOURNIGHTSCOUTURL.herokuapp.com # Add your own Nightscout URL here
+USEMMOL=true # true if you use mmol/l units. false if you use mg/dl
 
 
-CURRENTRESULT=$(curl --silent $NSURL/api/v1/entries/current)
-RESULTARRAY=($CURRENTRESULT)
+read -r -a RESULTARRAY <<< "$(curl --silent $NSURL/api/v1/entries/current)"
 
-TIMESTAMP=${RESULTARRAY[0]}
-TIMESTAMP=${TIMESTAMP//\.[0-9][0-9][0-9]/}
-EPOCHTS=`date -j -f "%FT%T%z" $TIMESTAMP +%s`
-EPOCHNOW=`date +%s`
-TIMEDIFF=`echo "($EPOCHNOW - $EPOCHTS)/(60)" | bc`
+TIMESTAMP=${RESULTARRAY[0]}  
+TIMESTAMP=${TIMESTAMP//\.[0-9][0-9][0-9]/} #Strip milliseconds from the timestamp. BSD date doesn't like it.
+EPOCHTS=$(date -j -f "%FT%T%z" "$TIMESTAMP" +%s) # Convert timestamp to epoch time
+EPOCHNOW=$(date +%s) # Convert current time to epoch time
+TIMEDIFF=$(((EPOCHNOW - EPOCHTS)/60)) #calculate the difference
 
 BG=${RESULTARRAY[2]}
 if $USEMMOL ; then
-	BG=`echo "scale=1; $BG / 18" | bc`
+	BG=$(echo "scale=1; $BG / 18" | bc) #Convert mg/dl to mmol/l
 fi
 
 
 case ${RESULTARRAY[3]} in
 	FortyFiveUp)
-		TREND="/"
+		TREND='/'
 		;;
 	FortyFiveDown)
-		TREND="\"
+		TREND='\'
 		;;
 	SingleUp)
-		TREND="/\"
+		TREND='/\'
 		;;
 	SingleDown)
-		TREND="\/"
+		TREND='\/'
 		;;
 	Flat)
-		TREND="->"
+		TREND='->'
 		;;
 	DoubleUp)
-		TREND="//\\"
+		TREND='//\\'
 		;;
 	DoubleDown)
-		TREND="\\//"
+		TREND='\\//'
 		;;
 	*)
-		TREND==${RESULTARRAY[3]}
+		TREND=${RESULTARRAY[3]}
 		;;
 esac
 
-echo "$BG $TREND ($TIMEDIFF m ago)"
+echo "$BG $TREND (${TIMEDIFF}m ago)"
 echo "---"
 echo "Go to Nightscout | href=$NSURL"
 echo "Reading taken: $TIMESTAMP"
